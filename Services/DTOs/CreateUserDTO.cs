aebd19e5d3f324fa8c17111f954a63ac3228c808
Alessandro <a.ghio86@gmail.com>
2025-03-18 22:11:26 +0100

    refactor: CreateUserDTO reverted to standard class

--- Services/DTOs/CreateUserDTO.cs
+++ Services/DTOs/CreateUserDTO.cs
@@ -2,19 +2,27 @@
 
 namespace contrarian_reads_backend.Services.DTOs;
 
-public record CreateUserDTO(
-    [property: Required] Guid Id,
-    [property: Required] string Username,
-    [property: Required]
-    [property: EmailAddress]
-    string Email,
-    string? ProfilePictureUrl,
-    DateTime CreatedAt,
-    [property: Required]
-    [property: StringLength(100, ErrorMessage = "The {0} must be at least {2} characters long.", MinimumLength = 6)]
-    [property: DataType(DataType.Password)]
-    string Password,
-    [property: DataType(DataType.Password)]
-    [property: Compare("Password", ErrorMessage = "The password and confirmation password do not match.")]
-    string ConfirmPassword,
-    string? Bio);
+public class CreateUserDTO
+{
+    [Required] public Guid Id { get; set; }
+
+    [Required] public string Username { get; set; } = null!;
+
+    [Required] [EmailAddress] public string Email { get; set; } = null!;
+
+    public string? ProfilePictureUrl { get; set; }
+
+    public DateTime CreatedAt { get; set; }
+
+    [Required]
+    [StringLength(100, ErrorMessage = "The {0} must be at least {2} characters long.", MinimumLength = 6)]
+    [DataType(DataType.Password)]
+    public string Password { get; set; } = null!;
+
+    [Required]
+    [DataType(DataType.Password)]
+    [Compare(nameof(Password), ErrorMessage = "The password and confirmation password do not match.")]
+    public string ConfirmPassword { get; set; } = null!;
+
+    public string? Bio { get; set; }
+}
